b33fadafbdd17e0997c32a2b2694ca7bf029fe1b
klikflux <42775673+Madtail@users.noreply.github.com>
2023-02-21 10:33:34 +0100

    Wordcount and input loop

--- textanalyzer/textanalyzer/main.cpp
+++ textanalyzer/textanalyzer/main.cpp
@@ -12,25 +12,42 @@ void incrementMap(std::string sentence, std::map<int, int> (&sentences))
 {
 	// Definitions
 	std::vector<int> keys;
-	int sentenceLength = sentence.size();
 
 	// Get keys
 	for (auto const& imap : sentences)
 		keys.push_back(imap.first);
 
+	// Count words:
+	int words = 0;
+	for (int i = 0; i < sentence.size(); i++)
+	{
+		if (sentence[i] == ' ')
+		{
+			words += 1;
+		}
+		else if (i == sentence.size() - 1)
+		{
+			words += 1;
+		}
+	}
+
 	// Loop through keys and compare
 	for (int i = 0; i < keys.size(); i++)
 	{
-		if (sentenceLength == keys[i])
+		if (words == keys[i])
 		{
 			std::map<int,int>::iterator it = sentences.find(keys[i]);
 			if (it != sentences.end())
 				it->second += 1;
 		}
 	}
+
+	std::cout << "Sentence: " << sentence << std::endl;
+	std::cout << "Nbr words: " << words << std::endl;
+	
 }
 
-void countSentences(std::stringstream& buffer, std::map<int, int> (&sentences))
+void countWords(std::stringstream& buffer, std::map<int, int> (&sentences))
 {
 	std::string text = buffer.str();
 	std::string sentence = "";
@@ -41,6 +58,7 @@ void countSentences(std::stringstream& buffer, std::map<int, int> (&sentences))
 	{
 		if (isupper(text[i]) && sentence.size() == 0)
 		{
+			std::cout << "Uppercase: " << text[i] << std::endl;
 			start = i;
 		}
 		else if (text[i] == '.')
@@ -50,6 +68,7 @@ void countSentences(std::stringstream& buffer, std::map<int, int> (&sentences))
 			{
 				sentence.push_back(text[j]);
 			}
+			std::cout << "Incremented!" << std::endl;
 			incrementMap(sentence, sentences);
 			sentence.clear();
 		}
@@ -61,38 +80,85 @@ void countSentences(std::stringstream& buffer, std::map<int, int> (&sentences))
 
 }
 
+std::stringstream readFile(std::string filename)
+{
+	std::stringstream buf;
+	std::ifstream t(filename);
+	buf << t.rdbuf();
+	if (buf.str().size() == 0)
+	{
+		std::string input;
+		std::cout << "Exception opening / reading file" << std::endl;
+		std::cout << "Try again? y/n" << std::endl;
+		std::cin >> input;
+		if (input == "y")
+		{
+			std::cout << "Input filename: ";
+			std::cin >> input;
+			readFile(input);
+		}
+		else
+		{
+			return buf;
+		}
+	}
+
+
+	return buf;
+}
+
 int main()
 {
 	// Variables
 	std::map<int, int> sentences;
-	
+	bool running = true;
+	std::stringstream buf1;
+
 	// Initialize map
 	for (int i = 1; i <= 100; i++)
 	{
 		sentences.insert({ i, 0 });
 	}
-
-	printf("Print keys!!!!!\n");
-	for (auto const& imap : sentences)
-		std::cout << imap.first << std::endl;
-
 	// Program loop
 	// while cond
-		// Input file name
-	std::ifstream t("file2.txt");
-	std::stringstream buf1;
-	buf1 << t.rdbuf();
-
-	countSentences(buf1, sentences);
-
-	// Print sentence lengths
-	for (int i = 0; i < sentences.size(); i++)
+	while (running)
 	{
-		std::map<int, int>::iterator it = sentences.find(sentences[i]);
-		if (it != sentences.end())
-		std::cout << "Length: " << it->first << " | " << "Count: " << it->second << std::endl;
-	}
+		// Input file name
+		std::string input;
+		std::cout << "Input filename: ";
+		std::cin >> input;
+		std::cout << "\n";
+		std::stringstream buf1 = readFile(input);
+		if (buf1.str().size() == 0)
+		{
+			running = false;
+		}
+		
+		if (running)
+		{
+			countWords(buf1, sentences);
 
+			for (auto const& imap : sentences)
+				std::cout << "Words: " << imap.first << " | " << "Count: " << imap.second << std::endl;
+
+
+			std::cout << "Analyze another file? y/n" << std::endl;
+			std::cin >> input;
+			if (input == "y")
+			{
+				running = true;
+			}
+			else
+			{
+				running = false;
+			}
+		}
+	}
+		
+	
+
+	
+	
 	//std::cout << buf1.str() << std::endl;
 	
 
